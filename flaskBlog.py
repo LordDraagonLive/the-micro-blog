489fa037891ca34f84c5e9ba20616717c2c8b721
Buddhi Adhikari <buddhiadikari@live.com>
2020-05-12 20:17:18 +0530

    added new routes

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'some name',
+        'title':'first post',
+        'content':'first post here',
+        'date_posted':'12/01/2020'
+    },
+     {
+        'author':'some other name',
+        'title':'Third post',
+        'content':'first post here',
+        'date_posted':'03/01/2020'
+    }
+]
+
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
