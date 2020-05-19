d62e92177077c768539c9d29d218a270a3600c38
Buddhi Adhikari <buddhiadikari@live.com>
2020-05-19 12:10:59 +0530

    configured register and login routes

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,30 +1,57 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c467bb47a41bbbd46ed1fd11c0842a09445eade8a51c5213095b7e5756b035f6'
 
 posts = [
     {
-        'author':'some name',
-        'title':'first post',
-        'content':'first post here',
-        'date_posted':'12/01/2020'
+        'author': 'some name',
+        'title': 'first post',
+        'content': 'first post here',
+        'date_posted': '12/01/2020'
     },
-     {
-        'author':'some other name',
-        'title':'Third post',
-        'content':'first post here',
-        'date_posted':'03/01/2020'
+    {
+        'author': 'some other name',
+        'title': 'Third post',
+        'content': 'first post here',
+        'date_posted': '03/01/2020'
     }
 ]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'buddhiadikari@live.com' and form.password.data == 'asdf':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
