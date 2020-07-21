3252ec9c5cab26cdef1037f35d6d584a84d70bb9
Buddhi Adhikari <buddhiadikari@live.com>
2020-07-21 20:21:26 +0530

    Added reset password via email

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c467bb47a41bbbd46ed1fd11c0842a09445eade8a51c5213095b7e5756b035f6'
@@ -11,5 +13,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ.get('LIVE_MAIL_USER')
+# app.config['MAIL_PASSWORD'] = os.environ.get('LIVE_MAIL_PASS')
+mail = Mail(app)
+
+
 
 from flaskblog import routes
