2887a607cd27d4fa8c394f0e15e3fd6baa5229e1
Buddhi Adhikari <buddhiadikari@live.com>
2020-07-21 21:50:40 +0530

    added all the utility functions

---
+++ flaskblog/users/utils.py
@@ -0,0 +1,39 @@
+import os
+import secrets
+from PIL import Image
+from flask import url_for
+from flask_mail import Message
+from flaskblog import app, mail
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    # f_name => _ to represent that it is unused
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    # form_picture.save(picture_path)
+
+    return picture_fn
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+            sender='noreply@demo.com', recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('users.reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    # print("This is the email : ")
+    # print(app.config['MAIL_USERNAME'])
+    # print("This is the password : ")
+    # print(app.config['MAIL_PASSWORD'])
+
+    mail.send(msg)
