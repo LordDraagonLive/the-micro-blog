5ee74b0a78d42975b6082f1a0c88b0981250da13
Buddhi Adhikari <buddhiadikari@live.com>
2020-07-22 14:16:03 +0530

    Added configuration class design pattern

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,31 +1,34 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'c467bb47a41bbbd46ed1fd11c0842a09445eade8a51c5213095b7e5756b035f6'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USERNAME'] = os.environ.get('LIVE_MAIL_USER')
-# app.config['MAIL_PASSWORD'] = os.environ.get('LIVE_MAIL_PASS')
-mail = Mail(app)
+
+mail = Mail()
 
 
+def create_app(config_class=Config):
+    
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-from flaskblog.users.routes import users
-from flaskblog.posts.routes import posts
-from flaskblog.main.routes import main
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+    return app
---
+++ flaskblog/config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config():
+    
+    SECRET_KEY = 'c467bb47a41bbbd46ed1fd11c0842a09445eade8a51c5213095b7e5756b035f6'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get('LIVE_MAIL_USER')
+    # MAIL_PASSWORD = os.environ.get('LIVE_MAIL_PASS')
+
+    
