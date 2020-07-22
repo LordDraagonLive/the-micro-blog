2f27367654501faf5f421b0aa532c14b304fa001
Buddhi Adhikari <buddhiadikari@live.com>
2020-07-22 14:14:44 +0530

    Changed app imports to complice with config class

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,9 +1,9 @@
 import os
 import secrets
 from PIL import Image
-from flask import url_for
+from flask import url_for, current_app
 from flask_mail import Message
-from flaskblog import app, mail
+from flaskblog import mail
 
 
 def save_picture(form_picture):
@@ -11,7 +11,7 @@ def save_picture(form_picture):
     # f_name => _ to represent that it is unused
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
 
     output_size = (125,125)
     i = Image.open(form_picture)
